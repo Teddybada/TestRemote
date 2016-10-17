a4f6d5a6d69aa713eaa08a35afadd7f215ebcbac
Teddybada <tidoone@yahoo.com>
2016-10-17 10:19:04 -0700

    Teddy added

--- src/Testing.java
+++ src/Testing.java
@@ -2,7 +2,7 @@
 public class Testing {
 
 	public static void main(String[] args) {
-		System.out.println("Check, check, one two. Teddy");
+		System.out.print("Check, check, one two. Teddy...");
 	}
 
 }
